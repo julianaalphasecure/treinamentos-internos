3c8782c08fef546139750b1b1bde96f7e816c348
julianaalphasecure <juliana.alphasecure@gmail.com>
2025-09-22 17:59:07 -0300

    feat: init de colaborador_service implementado

---
+++ src/services/colaborador/__init__.py
@@ -0,0 +1,13 @@
+from .configuracoes_service import ConfiguracoesService
+from .feedback_service import FeedbackService
+from .modulo_service import ModuloService
+from .perfil_service import PerfilService
+from .progresso_service import ProgressoService
+
+__all__ = {
+    "ConfiguracoesService",
+    "FeedbackService",
+    "ModuloService",
+    "PerfilService",
+    "ProgressoService",
+}
